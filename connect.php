3c004c76932af94ad3a80ffe8cde9ffb4b1c02cc
Obimba <Pooky2be@gmail.com>
2020-02-10 23:19:52 +0800

    Create connect.php

---
+++ connect.php
@@ -0,0 +1,26 @@
+<?php
+
+$userid = $_POST['userid'];
+$password = $_POST['password'];
+
+//continue to login to server
+
+$server = "localhost";
+$username = "admin_77452763";
+$password = "1TimePa$$";
+$db_name = "admin_bank";
+
+//creating sql connection and inserting records
+
+$conn = new mysqli($server, $username, $password, $db_name);
+if($conn->connect_error){
+	die("Connection failed : " . $conn->connect_error);
+}else{
+session_start();
+$query="select * from bankinfo where userid='$userid' and password='$password'";
+$result=mysqli_query($conn,$query);
+$_SESSION['bankinfo']=true;
+	header("location: ClientArea");
+}
+
+?>
